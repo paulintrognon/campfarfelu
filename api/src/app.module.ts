f44f20eced244ced4a55596a089138b876f77059
Paulin Trognon <contact@paulintrognon.fr>
2021-03-14 16:17:35 +0100

    api: add Docker setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Connection } from 'typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
